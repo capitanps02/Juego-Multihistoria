177c34f0097f8131b62aa5b1a66ef2d65e2c1efc
Pedro Sánchez Miguélez <pedro.smiguelez@alumnos.upm.es>
2026-09-16 23:49:53 +0200

    T5.5: activate canonical 20-23 microbatch overlay

--- src/content/events/20_23/index.ts
+++ src/content/events/20_23/index.ts
@@ -1,3 +1,13 @@
+import type { EventDefinition } from "../../../core/types.js";
 import { PRINCIPAL_EVENTS_20_23 } from "./principal-events.js";
 import { CONDITIONAL_EVENTS_20_23 } from "./conditional-events.js";
-export const EVENTS_20_23 = [...PRINCIPAL_EVENTS_20_23, ...CONDITIONAL_EVENTS_20_23];
+import { T55A_PRINCIPALS, T55A_CONDITIONALS } from "./canonical-t55-a.js";
+
+function replaceById(base: readonly EventDefinition[], replacements: readonly EventDefinition[]): EventDefinition[] {
+  const ids = new Set(replacements.map(event => event.id));
+  return [...base.filter(event => !ids.has(event.id)), ...replacements];
+}
+
+export const ACTIVE_PRINCIPAL_EVENTS_20_23 = replaceById(PRINCIPAL_EVENTS_20_23, T55A_PRINCIPALS);
+export const ACTIVE_CONDITIONAL_EVENTS_20_23 = replaceById(CONDITIONAL_EVENTS_20_23, T55A_CONDITIONALS);
+export const EVENTS_20_23 = [...ACTIVE_PRINCIPAL_EVENTS_20_23, ...ACTIVE_CONDITIONAL_EVENTS_20_23];
