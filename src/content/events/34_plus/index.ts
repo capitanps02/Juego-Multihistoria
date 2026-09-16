a512ac74395e4ca47a1b7ed9f93b2c1aab43619b
Pedro Sánchez Miguélez <pedro.smiguelez@alumnos.upm.es>
2026-09-16 23:13:36 +0200

    T5.36 apply terminal content overrides

--- src/content/events/34_plus/index.ts
+++ src/content/events/34_plus/index.ts
@@ -1,3 +1,7 @@
 import { PRINCIPAL_EVENTS_34_PLUS } from "./principal-events.js";
 import { CONDITIONAL_EVENTS_34_PLUS } from "./conditional-events.js";
+import { applyRetirementTerminalOverrides } from "./retirement-terminal-overrides.js";
+
+applyRetirementTerminalOverrides(PRINCIPAL_EVENTS_34_PLUS,CONDITIONAL_EVENTS_34_PLUS);
+
 export const EVENTS_34_PLUS=[...PRINCIPAL_EVENTS_34_PLUS,...CONDITIONAL_EVENTS_34_PLUS];
