15d4bf287b227d1effda30e0219c44fac87cdafe
Pedro Sánchez Miguélez <pedro.smiguelez@alumnos.upm.es>
2026-09-17 15:48:07 +0200

    chore(T5.11): remove temporary identity probe

--- scripts/print-t51-next-content-identity.mjs
+++
@@ -1,5 +0,0 @@
-import { EVENTS } from '../dist/content/events/index.js';
-import { contentIdentity } from '../dist/session/content-identity.js';
-
-const identity = await contentIdentity(EVENTS);
-console.log(`T51_NEXT_CONTENT_IDENTITY=${identity}`);
